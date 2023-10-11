dfce4f48790f5808d9be2a4b4977594920b24e30
Max Holden <maxholden11@gmail.com>
2023-10-11 07:06:35 -0400

    clean up

--- services/catalog/hldn-catalog/start.sh
+++ services/catalog/hldn-catalog/start.sh
@@ -2,4 +2,4 @@
 
 while ! curl -s identity-service:8180 > /dev/null; do echo waiting 3 seconds for identity-service; sleep 3; done;
 #while ! curl -s localhost:8180 > /dev/null; do echo waiting for identity-service; sleep 3; done;
-java -jar target/hldn-catalog-0.0.1-SNAPSHOT.jar
+java -jar target/hldn-catalog-0.0.1-SNAPSHOT.jar 
