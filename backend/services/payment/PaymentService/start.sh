b00bcb324ea5e130ea9f2d336430e0be669922ba
Max Holden <maxholden11@gmail.com>
2024-08-13 01:15:54 +0000

    aws back end working

--- backend/services/payment/PaymentService/start.sh
+++ backend/services/payment/PaymentService/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-while ! curl -s docker.for.mac.localhost:8090 > /dev/null; do echo waiting 10 seconds for identity-service; sleep 10; done;
+#DHOST=docker.for.mac.localhost
+DHOST=172.17.0.1
+
+while ! curl -s $DHOST:8090 > /dev/null; do echo waiting 10 seconds for identity-service; sleep 10; done;
 java -jar target/hldn-payment-0.0.1-SNAPSHOT.jar 
